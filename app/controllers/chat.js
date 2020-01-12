eef584451c55148818caa2216838b2b98465a6f6
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-11 21:55:42 -0300

    [Update] Validator + Show Erros in the page with EJS

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,6 +1,17 @@
 module.exports.iniciaChat = function(application, req, res){
 
     var dadosForm = req.body;
+
+    req.assert('apelido', 'O campo Nome/Apelido é obrigatório').notEmpty();
+    req.assert('apelido', 'O campo Nome/Apelido precisa ter entre 3 e 15 caracteres').len(3, 15)
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index', { validacao : errors })
+        return;
+    }
+
     console.log(dadosForm);
     res.render('chat');
 }
