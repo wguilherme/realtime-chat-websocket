336d1c55ca377aadd3c38cfb6394ff3aec5c7949
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-12 00:00:45 -0300

    [Feature] Add  Websocket Config to server + added server listen both http and websocket requires

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 var app = require('./config/server');
 
 // parametrizar a porta de escuta
-app.listen(80, function (){
+// passo2 atribuir a variável server para passar como parâmetro no socket.io
+var server = app.listen(80, function (){
     console.log('Servidor Online');
 })
+
+//importa o socket.io e passa onde irá escutar (server)
+// inteligência para:
+//receber tanto requisições http / websocket sejam recebidas e interpretadas
+require('socket.io').listen(server);
